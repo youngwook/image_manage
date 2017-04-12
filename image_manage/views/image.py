5137d7f8f769c48a6d8e8fb59b93782f44e5167f
youngwook <rongxux@gmail.com>
2017-04-13 03:12:12 +0900

    new admin views...

--- image_manage/views/image.py
+++ image_manage/views/image.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask import Blueprint, render_template, session, redirect, url_for, request, g
 from image_manage.database import db, Img, User, Platform, Os, Library, Application
 
@@ -11,15 +12,61 @@ def list():
                      User.usr_id).all()
     return render_template('admin/image/list.html', messages=messages)
 
-@mod.route('/create')
+@mod.route('/create', methods=['GET', 'POST'])
 def create():
-    os = Os.query.all()
-    lib = Library.query.all()
-    app = Application.query.all()
+
+    os = []
+    messages = db.session.query(Os.os_type.distinct().label('os_type')).all()
+    for m in messages:
+        os.append(db.session.query(Os).filter_by(os_type=m.os_type).all())
+
+    lib = []
+    messages = db.session.query(Library.lib_type.distinct().label('lib_type')).all()
+    for m in messages:
+        lib.append(db.session.query(Library).filter_by(lib_type=m.lib_type).all())
+
+    app = []
+    messages = db.session.query(Application.app_group.distinct().label('app_group')).all()
+    for m in messages:
+        app.append(db.session.query(Application).filter_by(app_group=m.app_group).all())
+
     pf = Platform.query.all()
+
+    if request.method == 'POST':
+        img_name = request.form['img_name']
+        img_type = request.form['img_type']
+        for o in os:
+            os_type = request.form['os']
+            if (os_type == o[0].os_type):
+                os_version =  request.form['os-{}'.format(os_type)]
+
+        for l in lib:
+            lib_type = request.form['lib']
+            if (lib_type == l[0].lib_type):
+                lib_version =  request.form['lib-{}'.format(lib_type)]
+
+        app_name = []
+        for a in app:
+            app_groups = request.form.getlist('application')
+            for app_group in app_groups:
+                if (app_group == a[0].app_group):
+                    app_name.append(a)
+
+        img = db.session.query(Img).filter_by(img_name=request.form['img_name']).first()
+        if img == None:
+            img = Img(img_name,img_type,"creating","---",datetime.utcnow(),2)
+            db.session.add(img)
+            db.session.commit()
+            return redirect(url_for('image.list'))
+        else:
+            error = 'the user name already exits'
+            return render_template('admin/image/create.html', pf=pf, os=os, lib=lib, app=app, error=error)
+
+        message = 'img name is {0}, img_type is {1}, os is {2}, lib is {3}, application is {4},,'.format(img_name, img_type, os_version, lib_version, app_name)
+        return render_template('admin/image/create.html', pf=pf, os=os, lib=lib, app=app, message=message)
     return render_template('admin/image/create.html', pf=pf, os=os, lib=lib, app=app)
 
-@mod.route('/delete')
+@mod.route('/delete', methods=['GET', 'POST'])
 def delete():
     messages = db.session.query(Img).join(User, Img.img_owner == User.usr_id) \
         .add_columns(Img.img_id, Img.img_name, Img.img_type, Img.img_state, Img.img_size, Img.img_time, User.usr_name,
@@ -30,10 +77,10 @@ def delete():
             img = db.session.query(Img).filter_by(img_id=i).first()
             db.session.delete(img)
             db.session.commit()
-        return render_template('admin/image/list.html')
+            return redirect(url_for('image.list'))
     return render_template('admin/image/delete.html', messages=messages)
 
-@mod.route('/uplaod')
+@mod.route('/uplaod', methods=['GET', 'POST'])
 def upload():
     messages = db.session.query(Img).join(User, Img.img_owner == User.usr_id) \
         .add_columns(Img.img_id, Img.img_name, Img.img_type, Img.img_state, Img.img_size, Img.img_time, User.usr_name,
@@ -43,5 +90,5 @@ def upload():
         for i in id:
             img = db.session.query(Img).filter_by(img_id=i).first()
 
-        return render_template('admin/image/list.html')
+            return redirect(url_for('image.list'))
     return render_template('admin/image/upload.html', messages=messages)
