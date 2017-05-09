33b9b57d6965ad7cd769ce0d4d9ad6f49eaa3d7e
youngwook <rongxux@gmail.com>
2017-05-09 15:10:58 +0900

    login working... ...

--- settings.py
+++ settings.py
@@ -1,10 +1,11 @@
+# -*- coding: utf-8 -*-
 import sys
 
 sys.stdout = sys.stderr
 sys.path.insert(0, '/var/www/myproject/image_manage/image_manage')
 sys.path.insert(0, '/var/www/myproject/image_manage')
 
-activate_this = '/var/www/myproject/env/bin/activate_this.py'
-execfile(activate_this, dict(__file__=activate_this))
+#activate_this = '/var/www/myproject/env/bin/activate_this.py'
+#execfile(activate_this, dict(__file__=activate_this))
 
 from image_manage import app as application
